e9aacfaa2eecdb9e0cbad8c207e0b6919459a433
hovakimyannn <harutyunhovakimyan7@gmail.com>
2026-04-03 00:24:33 +0400

    Fixed phone call border.

--- src/Twig/HomeSettingsTwigExtension.php
+++ src/Twig/HomeSettingsTwigExtension.php
@@ -7,6 +7,7 @@ use App\Repository\HomePageSettingsRepository;
 use App\Service\OpeningHoursSpecificationFromContactLines;
 use Twig\Extension\AbstractExtension;
 use Twig\Extension\GlobalsInterface;
+use Twig\TwigFilter;
 use Twig\TwigFunction;
 
 final class HomeSettingsTwigExtension extends AbstractExtension implements GlobalsInterface
@@ -30,6 +31,33 @@ final class HomeSettingsTwigExtension extends AbstractExtension implements Globa
         return [
             new TwigFunction('ango_opening_hours_specification', $this->openingHoursForJsonLd(...)),
         ];
+    }
+
+    public function getFilters(): array
+    {
+        return [
+            new TwigFilter('tel_uri', $this->telUri(...)),
+        ];
+    }
+
+    /**
+     * Human-readable phone for text; tel: href with digits (and leading + when present).
+     */
+    public function telUri(?string $phone): string
+    {
+        $phone = trim((string) $phone);
+        if ($phone === '') {
+            return '#';
+        }
+
+        $digitsOnly = preg_replace('/\D+/', '', $phone) ?? '';
+        if ($digitsOnly === '') {
+            return '#';
+        }
+
+        $prefix = str_starts_with($phone, '+') ? '+' : '';
+
+        return 'tel:'.$prefix.$digitsOnly;
     }
 
     /**
